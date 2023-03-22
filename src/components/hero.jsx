eb23b038bff386b52fb3b6dc65cfa44cadae70b2
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-03-22 17:28:43 +0700

    Add navbar and hero components

---
+++ src/components/hero.jsx
@@ -0,0 +1,61 @@
+import Carousel from 'react-bootstrap/Carousel';
+import hero1 from '../assets/image_1.jpeg';
+import hero2 from '../assets/image_4.jpeg';
+
+const Hero = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>SMA Negeri 3 Pontianak</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={ hero1 }
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hero2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Hero;
